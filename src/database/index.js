27ef0c92a1dfcc6d6b4ba0515c2d531642eef2fe
Henriques Salucamba <henriques.salucamba@interdigitos.co.ao>
2021-01-18 16:53:15 +0100

    Users register route created.

---
+++ src/database/index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+try {
+    mongoose.connect('mongodb://localhost/noderest', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+    
+} catch (error) {
+    console.log(`DB Connection error: ${error}`);
+}
+
+module.exports = mongoose
--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 7000;
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', (req, res) => res.send('Ola')) //To be removed
+require('./controllers/authController')(app);
 app.listen(PORT, () => console.log(`Server running at ${PORT} .......`));
